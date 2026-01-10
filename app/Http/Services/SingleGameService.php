30be68b318a577d59e17f2e93e49937577edcd51
Bruno Pereira <bruno762ix@gmail.com>
2026-01-10 14:01:06 -0300

    Get name of developer and single game page structure

--- app/Http/Services/SingleGameService.php
+++ app/Http/Services/SingleGameService.php
@@ -2,39 +2,60 @@
 
 namespace App\Http\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class SingleGameService{
+    public string $apiKey;
+    public function __construct()
+    {
+        $this->apiKey = env('API_KEY');
+    }
+
     public function getGameInfos($idGame){
-        $apikey = env('API_KEY');
-            $ch = curl_init();
-            curl_setopt_array($ch, [
-                CURLOPT_URL => 'https://api.thegamesdb.net/v1/Games/ByGameID?apikey='.$apikey.'&id='.$idGame,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_TIMEOUT => 10,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-                CURLOPT_HTTPHEADER => [
-                    'Accept: application/json'
-                ],
+
+        $response = Http::timeout(30)
+            ->retry(3, 1000)
+            ->acceptJson()
+            ->get('https://api.thegamesdb.net/v1/Games/ByGameID', [
+                'apikey' => $this->apiKey,
+                'id'     => $idGame,
             ]);
 
-            $response = curl_exec($ch);
+        if ($response->failed()) {
+            dd($response->body());
+        }
 
-            $response = json_decode($response, true);
+        $data = $response->json();
 
 
             //variaveis que precisam de outra busca
             $plataformId = $response['data']['games'][0]['platform'];
-            $developer = $response['data']['games'][0]['developers'][0];
+            $developerId = $response['data']['games'][0]['developers'][0];
             ///////////////////
-            curl_close($ch);
-
-            if ($response === false) {
-                dd(curl_error($ch));
-            }
                 //platform GET
                 //tem que pegar a lista de todas as plataformas e fazer uma filtragem pelo id
                 //mesma coisa com o developer
                 //pessima api pra ter que pegar todos os dados, poderia ter um endpoint que pegava literalmente todas as infos, enquanto tivesse
                 //outra que pegava mais filtrado sem tantos dados, pra um display mais simples, porem pra uma single page ter que fazer
                 //todos esses processos eh mt nada aver, ou to deixando passar algo bem burro e simples........ esperoq  nao
+            $this->getDeveloperName($developerId);
+    }
+
+
+
+    public function getDeveloperName($developerId){
+        $response = Http::timeout(30)
+            ->retry(3, 1000)
+            ->acceptJson()
+            ->get('https://api.thegamesdb.net/v1/Developers', [
+                'apikey' => $this->apiKey
+            ]);
+
+        if ($response->failed()) {
+            dd($response->body());
+        }
+        
+        $data = $response->json();
+        return $data['data']['developers'][$developerId]['name'];
     }
 }
